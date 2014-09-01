882760ba7f86eb25929b3e1080f8131886afcdc9
scobb00 <scobb00@users.noreply.github.com>
2014-09-01 13:06:21 -0400

    Beer Wine Scotch

--- src/button_click.c
+++ src/button_click.c
@@ -1,4 +1,5 @@
 #include <pebble.h>
+#include <pebble_fonts.h>
 
 static Window *window;
 //static TextLayer *text_layer;
@@ -6,19 +7,24 @@ TextLayer *text_beer_layer;
 TextLayer *text_oclock_layer;
 Layer *line_layer;
 
+static GFont beer_font;
+
+#define FONT_BEER_TIME RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_36
+
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) 
 {
-  text_layer_set_text(text_beer_layer, "Select");
+  text_layer_set_text(text_beer_layer, "Scotch");
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) 
 {
-  text_layer_set_text(text_beer_layer, "Upscale");
+  text_layer_set_text(text_beer_layer, "Wine");
 }
  
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) 
 {
-  text_layer_set_text(text_beer_layer, "Down");
+  text_layer_set_text(text_beer_layer, "Beer");
 }
 
 void line_layer_update_callback(Layer *layer, GContext* ctx) 
@@ -39,13 +45,20 @@ static void window_load(Window *window)
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Get our fonts
+  beer_font = fonts_load_custom_font(resource_get_handle(FONT_BEER_TIME));
+  // big_font = fonts_load_custom_font(resource_get_handle(FONT_BIG_TIME));
+  // seconds_font = fonts_load_custom_font(resource_get_handle(FONT_SECONDS));
+  // laps_font = fonts_load_custom_font(resource_get_handle(FONT_LAPS));
+
   // text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
   text_beer_layer = text_layer_create(GRect(8, 52, 144-8, 168-52));
   text_layer_set_text(text_beer_layer, "Scotch");
   //text_layer_set_text_alignment(text_beer_layer, GTextAlignmentCenter);
   text_layer_set_text_color(text_beer_layer, GColorWhite);
   text_layer_set_background_color(text_beer_layer, GColorClear);
-  text_layer_set_font(text_beer_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_36)));
+  // text_layer_set_font(text_beer_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_36)));
+  text_layer_set_font(text_beer_layer, beer_font);
   layer_add_child(window_layer, text_layer_get_layer(text_beer_layer));
   
   // text_beer_layer
@@ -64,7 +77,8 @@ static void window_load(Window *window)
   text_layer_set_text(text_oclock_layer, "O'Clock");
   text_layer_set_text_color(text_oclock_layer, GColorWhite);
   text_layer_set_background_color(text_oclock_layer, GColorClear);
-  text_layer_set_font(text_oclock_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_36)));
+  // text_layer_set_font(text_oclock_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_36)));
+  text_layer_set_font(text_oclock_layer, beer_font);
   layer_add_child(window_layer, text_layer_get_layer(text_oclock_layer));
 
   // Line layer
@@ -80,12 +94,18 @@ static void window_unload(Window *window)
 {
   //text_layer_destroy(text_layer);
   text_layer_destroy(text_beer_layer);
+  layer_destroy(line_layer);
+  text_layer_destroy(text_oclock_layer);
+//	text_layer_destroy(text_beer_layer);
+//	fonts_unload_custom_font(big_font);
+//	fonts_unload_custom_font(seconds_font);
+	fonts_unload_custom_font(beer_font);
 }
 
 static void handle_init(void) {
   window = window_create();
   window_set_background_color(window, GColorBlack);
- window_set_fullscreen(window, true);
+  window_set_fullscreen(window, true);
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) 
   {
